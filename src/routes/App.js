7c95665964371ba9232743fc3be6a59b55f868ab
claualegrec <113923844+claualegrec@users.noreply.github.com>
2023-01-16 13:56:17 -0700

    Ahora se puede acceder al login

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+/* Routes */
 import Home from "../containers/Home";
+import Login from "../containers/Login";
 
 const App = () =>  (
   <BrowserRouter>
     <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login}/>
   </BrowserRouter>)
 
 export default App;
