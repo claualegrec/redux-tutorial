62fd2bff31ab553de96e5fe2b52f1b7ec44f9b84
claualegrec <113923844+claualegrec@users.noreply.github.com>
2023-01-20 17:55:27 -0700

    provider

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,6 @@ const Layout = ({ children }) => (
         <Header />
             {children}
         <Footer />
-
     </div>
 );
 
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./routes/App";
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
