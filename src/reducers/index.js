5a2ed2191206d44c1dcaab93917227b5949521d9
claualegrec <113923844+claualegrec@users.noreply.github.com>
2023-01-26 11:01:32 -0700

    Validacion de no elementos duplicados en favoritos

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,15 @@ import { ActionTypes } from "@redux-devtools/core";
 const reducer = (state, action) => {
   switch (action.type) {
     case "SET_FAVORITE":
+      const exist = state.myList.find((item) => item.id === action.payload.id);
+
+      if (exist) return { ...state };
+
       return {
         ...state,
         myList: [...state.myList, action.payload],
       };
+      
     case "DELETE_FAVORITE":
       return {
         ...state,
