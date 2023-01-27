8524c5ff1929979066cf94e942e1288b7ac1fa65
claualegrec <113923844+claualegrec@users.noreply.github.com>
2023-01-27 09:49:12 -0700

    Implementacion de clasnames en search

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ const Home = ({ myList, trends, originals }) => {
   return (
     <>
       <Header />
-      <Search />
+      <Search isHome />
       {myList.length > 0 && (
         <Categories title="Mi Lista">
           <Carousel>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import reducer from './reducers';
 import App from "./routes/App";
 
@@ -251,7 +252,9 @@ const initialState = {
   ],
 };
 
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
